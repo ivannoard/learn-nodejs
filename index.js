4972c7b93be4590fda1fc475573c0cd121c02d1f
ivan <ivannovaa.24@gmail.com>
2022-08-25 21:19:38 +0700

    section 3-nodejs

---
+++ index.js
@@ -0,0 +1,5 @@
+import { writeToFile } from './write.js'
+
+writeToFile('hello.log', 'ivan nova ardiansyah')
+
+console.info('hello from index.js')
---
+++ lodash.js
@@ -0,0 +1,6 @@
+import _ from 'lodash';
+
+const name = 'IVAN NOVA ARDIANSYAH'
+const result = _.camelCase(name)
+
+console.info(result)
